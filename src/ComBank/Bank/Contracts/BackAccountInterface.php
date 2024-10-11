16253070f98b8d2c5686a9cff357bc7d1ec4f1a0
perita23 <hugobedmar10@gmail.com>
2024-10-11 18:33:03 +0200

    BankAccInterface - methods defined

--- src/ComBank/Bank/Contracts/BackAccountInterface.php
+++ src/ComBank/Bank/Contracts/BackAccountInterface.php
@@ -1,4 +1,6 @@
-<?php namespace ComBank\Bank\Contracts;
+<?php
+
+namespace ComBank\Bank\Contracts;
 
 /**
  * Created by VS Code.
@@ -17,5 +19,12 @@ interface BackAccountInterface
     const STATUS_OPEN = 'OPEN';
     const STATUS_CLOSED = 'CLOSED';
 
-   
+    public function transaction($BankTransaction): void;
+    public function openAccount(): bool;
+    public function reopenAccount(): void;
+    public function closeAccount(): void;
+    public function getBalance(): float;
+    public function getOverdraft(): OverdraftInterface;
+    public function applyOverdraft(OverdraftInterface $overdraft): void;
+    public function setBalance(float $amount): void;
 }
